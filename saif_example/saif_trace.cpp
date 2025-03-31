445aa4c2e6f934fb0be10204b3bfdc3defcddb06
Mateusz Gancarz <mgancarz@antmicro.com>
2025-03-31 13:28:18 +0200

    add license to examples cpp

--- saif_example/saif_trace.cpp
+++ saif_example/saif_trace.cpp
@@ -1,10 +1,18 @@
-// -*- mode: C++; c-file-style: "cc-mode" -*-
+// Copyright 2025 Antmicro <antmicro.com>
 //
-// DESCRIPTION: Verilator: Verilog Test module
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2025 by Wilson Snyder.
-// SPDX-License-Identifier: CC0-1.0
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #include <verilated.h>
 #include <verilated_saif_c.h>
